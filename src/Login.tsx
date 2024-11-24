bd7d6e2acadd6b76b73157b7ddc4f7fa14c90561
Ruben Lellouche <96834866+rublel@users.noreply.github.com>
2024-11-24 22:06:44 +0200

    Update Login.tsx

--- src/Login.tsx
+++ src/Login.tsx
@@ -7,7 +7,7 @@ export function Login() {
   const code = new URLSearchParams(location.search).get("code");
 
   const state = Math.random().toString(36).substring(7);
-  const BASE_URI = "http://localhost:3000/login";
+  const BASE_URI = "https://shopify-clienteling-frontent-47ksogvjc-digital-web-concept.vercel.app/login";
 
   const url = `https://balink-demo-shop.myshopify.com/admin/oauth/authorize?client_id=${process.env.REACT_APP_SHOPIFY_CLIENT_ID}&scope=write_products&redirect_uri=${BASE_URI}&state=${state}&grant_options[]=per-user`;
 
