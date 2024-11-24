0dabb467c24aba02960809504e1fc6a26ac4fd1a
Ruben Lellouche <82046086+RubenLellouche@users.noreply.github.com>
2024-11-25 00:15:06 +0200

    Update Login.tsx

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,14 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useLocation } from "react-router-dom";
+import { useState } from "react";
 import axios from "axios";
 
 export function Login() {
   const location = useLocation();
   const code = new URLSearchParams(location.search).get("code");
-
   const state = Math.random().toString(36).substring(7);
   const BASE_URI = "https://shopify-clienteling-frontent.vercel.app/login";
+
+  const [accessToken, setAccessToken] = useState("");
+  const [userInformation, setUserInformation] = useState({} as any);
 
   const url = `https://balink-demo-shop.myshopify.com/admin/oauth/authorize?client_id=${process.env.REACT_APP_SHOPIFY_CLIENT_ID}&scope=write_products&redirect_uri=${BASE_URI}&state=${state}&grant_options[]=per-user`;
   let data;
@@ -22,11 +25,17 @@ export function Login() {
           client_id: process.env.REACT_APP_SHOPIFY_CLIENT_ID,
           client_secret: process.env.REACT_APP_SHOPIFY_CLIENT_SECRET,
           code,
+        },
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Access-Control-Allow-Origin": "*",
+          },
         }
       )
       .then((res) => {
-        data = res;
-        console.log(res);
+        setAccessToken(res.data.access_token);
+        setUserInformation(res.data.associated_user);
       });
   }
 
@@ -35,8 +44,13 @@ export function Login() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>{code}</code>
-          <code>{JSON.stringify(data)}</code>
+          <code>code: {code}</code>
+          <code>token: {accessToken}</code>
+
+          <code>
+            user: {userInformation.first_name} {userInformation.last_name}
+          </code>
+          <code>email: {userInformation.email}</code>
         </p>
       </header>
     </div>
