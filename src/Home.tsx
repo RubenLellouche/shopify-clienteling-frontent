00a3d06e2977ae30e02d4e564599cb12d7c98cd6
ruben lellouche <rubenl@balink.net>
2024-11-25 01:15:16 +0200

    Update routing in App to redirect all paths to Home component

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,17 +1,13 @@
 import React from "react";
 import logo from "./logo.svg";
-import { useLocation } from "react-router-dom";
 
 export function Home() {
-  const location = useLocation();
-  const access_token = new URLSearchParams(location.search).get("access_token");
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>{access_token}</code>
+          <code>Hello World</code>
         </p>
       </header>
     </div>
