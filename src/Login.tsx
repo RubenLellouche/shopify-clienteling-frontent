0167b5d9799b0776ac104a6219db63e119bf13cc
ruben lellouche <rubenl@balink.net>
2024-11-25 02:07:05 +0200

    Update Login component to use dynamic shop parameter for OAuth URL

--- src/Login.tsx
+++ src/Login.tsx
@@ -3,27 +3,20 @@ import "./App.css";
 import { useLocation } from "react-router-dom";
 
 export function Login() {
-  const location = useLocation();
-  const code = new URLSearchParams(location.search).get("code");
   const state = Math.random().toString(36).substring(7);
+  const shop = new URLSearchParams(useLocation().search).get("shop");
   const REDIRECT_BASE_URI =
     "https://shopify-clienteling-backend-1f4b633c8cf5.herokuapp.com/shopify/oauth/access_token";
 
-  const url = `https://balink-demo-shop.myshopify.com/admin/oauth/authorize?client_id=${process.env.REACT_APP_SHOPIFY_CLIENT_ID}&scope=write_products&redirect_uri=${REDIRECT_BASE_URI}&state=${state}&grant_options[]=per-user`;
+  const url = `https://${shop}/admin/oauth/authorize?client_id=${process.env.REACT_APP_SHOPIFY_CLIENT_ID}&scope=write_products&redirect_uri=${REDIRECT_BASE_URI}&state=${state}&grant_options[]=per-user`;
 
   window.location.href = url;
 
   return (
-    console.log({ code, url }),
-    (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            <code>code: {code}</code>
-          </p>
-        </header>
-      </div>
-    )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
   );
 }
