97c1dc4d14724762561d41f1e72bcf885897b962
ruben lellouche <rubenl@balink.net>
2024-12-02 23:36:58 +0200

    Update Home component to handle access token and navigate to login if not present

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,18 +1,25 @@
-import React from "react";
 import logo from "./logo.svg";
-import { useLocation } from "react-router-dom";
-import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export function Home() {
+  const navigate = useNavigate();
   const query = new URLSearchParams(useLocation().search);
   const shopParam = query.get("shop");
+  const accessToken = localStorage.getItem("accessToken");
+  const isAccessTokenInUrl = query.get("access_token");
 
+  if (isAccessTokenInUrl && !accessToken) {
+    localStorage.setItem("accessToken", isAccessTokenInUrl);
+    console.log(`Access token: ${isAccessTokenInUrl}`);
+  } else {
+    navigate("/login");
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>Hello ${shopParam}!</code>
+          <code>Hello ${shopParam} isAccessTokenInUrl!</code>
         </p>
       </header>
     </div>
